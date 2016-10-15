24751dd621e4d14bdb33c859720c39a74c2aa1e6
luongvanduc04101996 <luongvanduc04101996@gmail.com>
2016-10-15 14:47:20 +0700

    Create Bai04Level01.c

---
+++ Bai04Level01.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+void main(){
+int chieuDai,chieuRong;
+printf("Nhap chieu dai va chieu rong:\n");
+scanf("%d%d",&chieuDai,&chieuRong);
+printf("Chu vi:%d\nDien tich:%d",(chieuDai+chieuRong)*2,chieuDai*chieuRong);
+}
