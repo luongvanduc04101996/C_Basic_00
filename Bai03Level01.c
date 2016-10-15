9f61dcec6d8bb8e487816ffc96560c1b54504aa8
luongvanduc04101996 <luongvanduc04101996@gmail.com>
2016-10-15 14:42:37 +0700

    Create Bai03Level01.c

---
+++ Bai03Level01.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main(){
+float canh,chuVi,dienTich;
+printf("Nhap canh cua hinh vuong: \n");
+scanf("%f",&canh);
+chuVi=4*canh;
+dienTich=canh*canh;
+printf("Chu vi: %.2f\nDien tich: %.2f",chuVi,dienTich);
+}
